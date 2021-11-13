f24ead5edf445f172b73199bb014309ab2643aec
alexmatandos <amatand@clemson.edu>
2021-11-12 20:20:06 -0500

    craigslist cars scrapper

---
+++ craigslist_parse.py
@@ -0,0 +1,25 @@
+import os
+from bs4 import BeautifulSoup
+import time
+import glob
+
+for file in glob.glob("car_html_files/*.html"):
+	f = open(file, encoding = "UTF-8")
+	soup = BeautifulSoup(f.read(), "html.parser")
+	prices = soup.find("span", {"class": "price"})
+	if prices == None:
+		price = "N/A"
+	else:
+		price = prices.text.replace("$", "").replace(",", "")
+	
+	infos = soup.find_all("p", {"class": "attrgroup"})
+	vehicle = infos[0].text
+	year = vehicle[0:5]
+	name = vehicle[6: len(vehicle)]
+	
+	more_info = infos[1]
+	span = more_info.find_all("span")
+	print(span[1])
+
+
+	
---
+++ craigslist_request.py
@@ -0,0 +1,40 @@
+import urllib.request
+import os
+from bs4 import BeautifulSoup
+import time
+import glob
+
+if not os.path.exists("html_files"):
+	os.mkdir("html_files")
+
+if not os.path.exists("car_html_files"):
+	os.mkdir("car_html_files")
+
+for i in range(5):
+	index = i*120
+	f = open("html_files/page" + str(index) + ".html", "wb")
+	response = urllib.request.urlopen("https://greenville.craigslist.org/d/cars-trucks-by-owner/search/cto?s=" + str(index))
+	html = response.read()
+	f.write(html)
+	f.close()
+	print(index)
+
+for file in glob.glob("html_files/*.html"):
+	f = open(file, encoding = "UTF-8")
+	soup = BeautifulSoup(f.read(), "html.parser")
+	f.close()
+
+	results = soup.find("ul", {"id": "search-results"})
+	cars = results.find_all("li")
+
+	for car in cars:
+		tag = car.find("a")
+		url = tag.get("href")
+		link = url.replace("https://", "").replace("/", "")
+		
+		f = open("car_html_files/" + str(link), "wb")
+		response = urllib.request.urlopen(url)
+		html = response.read()
+		f.write(html)
+		f.close()
+		print(link)
