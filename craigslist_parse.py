3b4aac3b2d8d0bd715077647b17615a015561962
alexmatandos <amatand@clemson.edu>
2021-11-21 14:32:42 -0500

    update 11/21/2021

--- craigslist_parse.py
+++ craigslist_parse.py
@@ -2,7 +2,7 @@ import pandas
 import os
 from bs4 import BeautifulSoup
 import glob
-import re
+from datetime import datetime
 
 if not os.path.exists("parsed_files"):
 	os.mkdir("parsed_files")
@@ -10,65 +10,43 @@ if not os.path.exists("parsed_files"):
 df = pandas.DataFrame()
 
 for file in glob.glob("car_html_files/*.html"):
+	result_list = {}
 	f = open(file, encoding = "UTF-8")
 	soup = BeautifulSoup(f.read(), "html.parser")
+	
 	prices = soup.find("span", {"class": "price"})
 	if prices == None:
-		price = "N/A"
+		price = ""
 	else:
 		price = prices.text.replace("$", "").replace(",", "")
+	result_list['price'] = price		
 	
-	infos = soup.find_all("p", {"class": "attrgroup"})
-	vehicle = infos[0].text
-	year = vehicle[0:5].replace("\n", "").replace("\r", "")
-	name = vehicle[6: len(vehicle)]
-	name_split = name.split()
-	manufacturer = name_split[0]
-	model = name.replace(manufacturer, "")
-	more_info = infos[1]
-	spans = more_info.find_all("span")
+	attrgroups = soup.find_all("p", {"class" : "attrgroup"})
+	model = attrgroups[0].text.replace('\n', '').replace('\r','')
+	car_age = datetime.now().year - int(model[0:5])
 	
-	for span in spans:
-		values = span.find("b")
-		if values == None:
-			value = "N/A"
-			category = "N/A"
+	#print(model[0:5])
+	#print(datetime.now().year)
+	#print(car_age)
+	
+	result_list['model'] = model
+	result_list['price'] = price
+	result_list['car age'] = car_age
+	
+	span = attrgroups[1].find_all("span")
+	for x in span:
+		value = x.find("b")
+		if value != None:
+			value = value.text
+			attribute = x.text.replace(value, "").strip()
 		else:
-			value = values.text
-			category = span.text.replace(value, "").replace(":","")
-			category = category.split()
-		
-		if category == ['fuel']:
-			fuel = value
-			
-		if category == ['odometer']:
-			mileage = value
-			
-		if category == ['transmission']:
-			transmission = value
-			
-		if category == ['title', 'status']:
-			title = value
-			
-		if category == ['condition']:
-			condition = value
-			
-		if category == ['cylinders']:
-			engine = value
+			value = ""
+		result_list[attribute] = value
 
-	df = df.append({
-		'Year': year,
-		'Manufacturer': manufacturer,
-		'Model': model,
-		'Price': price,
-		'Fuel': fuel,
-		'Mileage': mileage,
-		'Transmission': transmission,
-		'Title': title
-		}, ignore_index = True)
-	df = df.drop_duplicates()
+	if result_list:
+		df = df.append(result_list, ignore_index = True)
 
-df.to_csv("parsed_files/craigslist_cars.csv")	
+df.to_csv("parsed_files/craigslist_cars.csv")
 
 
 		
